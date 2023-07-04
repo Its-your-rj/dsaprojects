33ed4136e3a755c9e874190b9bfc9ea4e5ea3d4c
Its-your-rj <131703345+Its-your-rj@users.noreply.github.com>
2023-07-04 10:52:50 +0530

    Add files via upload

---
+++ addbinary.cpp
@@ -0,0 +1,135 @@
+#include <iostream>
+#include<bits/stdc++.h>
+/* program to do addition of two binary numbers*/
+
+using namespace std;
+int reverse(int n)
+{
+    int ans=0;
+    while(n>0)
+    {
+        int ld=n%10;
+        ans=ans*10+ld;
+        n/=10;
+    }
+    return ans;
+}
+int addbinary(int a,int b)
+{
+    int ans=0;
+    int carry=0;
+    while(a>0 && b>0)
+    {
+        if(a%2==0 && b%2==0 )
+        {
+            ans=ans*10+carry;
+            carry=0;
+        }
+        else if((a%2==0 && b%2==1) || (a%2==1 &&b%2==0))
+        {
+            if(carry==1)
+            {
+                ans=ans*10+0;
+                carry=1;
+            }
+            else
+            {
+                ans=ans*10+1;
+                carry=0;
+            }
+        }
+        else
+        {
+            
+            ans=ans*10+carry;
+            carry=1;
+        }
+        a/=10;
+        b/=10;
+    }
+    while(a>0)
+    {
+        if(carry==1)
+        {
+            ans=ans*10+0;
+            carry=1;
+        }
+        else
+        {
+            ans=ans*10+1;
+            carry=0;
+        }
+    }
+    while(a>0)
+    {
+        if(carry==1)
+        {
+            if(a%2==1)
+            {
+                ans=ans*10+0;
+                carry=1;
+            }
+            else
+            {
+                ans=ans*10+1;
+                carry=0;
+            }
+        }
+        else
+        {
+            ans=ans*10+(a%2);
+        }
+        a/=10;
+    }
+    while(b>0)
+    {
+        if(carry==1)
+        {
+            ans=ans*10+0;
+            carry=1;
+        }
+        else
+        {
+            ans=ans*10+1;
+            carry=0;
+        }
+    }
+    while(b>0)
+    {
+        if(carry==1)
+        {
+            if(b%2==1)
+            {
+                ans=ans*10+0;
+                carry=1;
+            }
+            else
+            {
+                ans=ans*10+1;
+                carry=0;
+            }
+        }
+        else
+        {
+            ans=ans*10+(b%2);
+        }
+        b/=10;
+    }
+    if(carry==1)
+    {
+        ans=ans*10+1;
+        
+    }
+    ans=reverse(ans);
+    
+}
+
+int main()
+{
+    int a,b;
+    cin>>a>>b;
+    cout<<addbinary(a,b)<<endl;
+
+
+    return 0;
+}
